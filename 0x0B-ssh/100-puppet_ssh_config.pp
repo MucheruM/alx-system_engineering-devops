ab897c914670383d28a25635a795e492ccb8bd37
MucheruM <petermucheru420@gmail.com>
2024-04-23 19:41:52 -0700

    Create a configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,11 @@
-IdentityFile ~/.ssh/school
+# Puppet manifest to ensure SSH client configuration
+
+$config = "IdentityFile ~/.ssh/school
 PubKeyAuthentication yes
 PasswordAuthentication no
+"
+
+file {'/etc/ssh/ssh_config':
+  ensure  => present,
+  content => $config,
+}
